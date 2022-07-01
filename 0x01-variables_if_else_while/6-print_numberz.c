b9604e1961f743822667676cc6cf1d0f2f11f476
Ousman <usman.traid@gmail.com>
2022-07-01 11:02:12 -0700

    print numbers from code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	};
+
+	putchar('\n');
+
+	return (0);
+}
