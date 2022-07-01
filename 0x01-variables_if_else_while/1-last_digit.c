e3bb83f7586257636740eb74a388ac877c600b36
Ousman <usman.traid@gmail.com>
2022-07-01 08:50:46 -0700

    correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 0\n", n, n % 10);
 	} else
 	{
-		printf("Last digit of %d is %d and is less than 6and not 0\n" n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	};
 
 	return (0);
