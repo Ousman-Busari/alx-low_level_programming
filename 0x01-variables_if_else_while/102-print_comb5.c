5da086550c26fc303a57c3df4e178c3b44d701f5
Ousman <usman.traid@gmail.com>
2022-07-02 10:15:41 -0700

    final correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,21 +16,21 @@ int main(void)
 
 		for (first_pair_2 = '0'; first_pair_2 <= '9'; first_pair_2++)
 		{
-			int second_pair_1;
+			int second_pair_1, temp_2nd;
 
-			if (first_pair_2 != '9')
-				second_pair_1 = first_pair_1;
+			if (first_pair_2 == '9')
+				temp_2nd = first_pair_1 + 1;
 			else
-				second_pair_1 = first_pair_1 + 1;
-			for (; second_pair_1 <= '9'; second_pair_1++)
+				temp_2nd = first_pair_1;
+			for (second_pair_1 = temp_2nd; second_pair_1 <= '9'; second_pair_1++)
 			{
-				int second_pair_2;
+				int second_pair_2, temp_2nd_2nd;
 
-				if (first_pair_2 != '9')
-					second_pair_2 = first_pair_2 + 1;
+				if (first_pair_2 == '9' || first_pair_1 == '0')
+					temp_2nd_2nd = '0';
 				else
-					second_pair_2 = '0';
-				for (; second_pair_2 <= '9'; second_pair_2++)
+					temp_2nd_2nd = first_pair_2 + 1;
+				for (second_pair_2 = temp_2nd_2nd; second_pair_2 <= '9'; second_pair_2++)
 				{
 					putchar(first_pair_1);
 					putchar(first_pair_2);
