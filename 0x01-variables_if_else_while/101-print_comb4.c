8e30b1cc245d3cf11711bea0aaa7424e2fd68680
Ousman <usman.traid@gmail.com>
2022-07-02 06:32:39 -0700

    three digits combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	int d;
+
+	for (d = '0'; d < '8'; d++)
+	{
+		int first_temp = d + 1;
+
+		for (; first_temp <= '8'; first_temp++)
+		{
+			int second_temp = first_temp + 1;
+
+			for (; second_temp <= '9'; second_temp++)
+			{
+				putchar(d);
+				putchar(first_temp);
+				putchar(second_temp);
+				if (d != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				};
+			};
+		};
+	};
+
+	putchar(' ');
+
+	return (0);
+}
