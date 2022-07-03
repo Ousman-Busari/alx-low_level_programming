9985b1758adfb2395f45fbd7a29d004265073ba5
Ousman <usman.traid@gmail.com>
2022-07-03 04:19:01 -0700

    nested if solution

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -28,10 +28,13 @@ int main(void)
 
 				if (first_pair_2 == '9')
 					temp_2nd_2nd = '0';
-				else if (first_pair_1 == '0' && second_pair_1 > '0')
-					temp_2nd_2nd = '0';
-				else if (first_pair_1 > '0' && first_pair_2 == '0' && second_pair_1 > '0')
-					temp_2nd_2nd = '0';
+				else if (first_pair_2 == '0')
+				{
+					if (first_pair_1 > '0' && second_pair_1 > '1')
+						temp_2nd_2nd = '0';
+					else 
+						temp_2nd_2nd = '1';
+				}
 				else
 					temp_2nd_2nd = first_pair_2 + 1;
 				for (second_pair_2 = temp_2nd_2nd; second_pair_2 <= '9'; second_pair_2++)
