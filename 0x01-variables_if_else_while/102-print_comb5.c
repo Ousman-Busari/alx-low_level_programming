59406c6c6b110207b003248b7b18a2c3b97223b2
Ousman <usman.traid@gmail.com>
2022-07-03 03:38:54 -0700

    new correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -31,7 +31,7 @@ int main(void)
 				else if (first_pair_1 == '0' && second_pair_1 > '0')
 					temp_2nd_2nd = '0';
 				else if (first_pair_1 > '0' && first_pair_2 == '0')
-					temp_2nd_2nd = '0';
+					temp_2nd_2nd = '1';
 				else
 					temp_2nd_2nd = first_pair_2 + 1;
 				for (second_pair_2 = temp_2nd_2nd; second_pair_2 <= '9'; second_pair_2++)
