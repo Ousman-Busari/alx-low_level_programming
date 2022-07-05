4a594dc57907be979b223fd96e931b48a003f101
Ousman <usman.traid@gmail.com>
2022-07-05 07:48:10 -0700

    printing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void main()
+{
+	printf("_putchar\n");
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the main readme for this fucntion and nested llops projects. And it's located here in the root directory
