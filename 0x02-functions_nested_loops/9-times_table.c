300045b7d4cf5f10f9c81c8393887cb7817fde57
Ousman <usman.traid@gmail.com>
2022-07-05 15:49:31 -0700

    multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,12 +22,14 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
 				}
 
 			} else
 			{
-				_putchar((n / 10) + '0');
+				if (n < 10)
+					_putchar(' ');
+				else
+					_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 				if (i != 9)
 				{
