4925a1ad9ab206ae65431577d780513d6450f210
Ousman <usman.traid@gmail.com>
2022-07-05 11:07:58 -0700

    print ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int 1;
+
+	i = 0;
+	while (1 <= 10)
+	{
+		print_alphabet();
+		i++;
+	}
+	_putchar('\n');
+}
