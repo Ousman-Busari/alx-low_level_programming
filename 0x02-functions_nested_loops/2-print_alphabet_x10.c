4a460cdba43263db9984ce75ff6f1b85253caaac
Ousman <usman.traid@gmail.com>
2022-07-05 11:18:44 -0700

    print ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,11 +16,8 @@ void print_alphabet_x10(void)
 		char letter;
 
 		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			print_alphabet();
-			_putchar('\n');
-		}
+			_putchar(letter);
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
