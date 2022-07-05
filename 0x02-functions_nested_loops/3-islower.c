dd2e9b1d3b300773c05e4adfc781ab32cfde8b58
Ousman <usman.traid@gmail.com>
2022-07-05 11:43:49 -0700

    isLower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - determine lower or upper case character
+ * return 1 if uppercase, 0 otherwise
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	int a;
+	int result;
+
+	if (a >= 65 && a <= 90)
+		result = 0;
+	else if (a >= 97 && a <= 1220)
+		result = 1;
+
+	return(result);
+}
