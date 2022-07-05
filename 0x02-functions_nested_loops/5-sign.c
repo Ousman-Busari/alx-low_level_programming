51a65daef7277d08fdc43692b70a5e5ff43535f3
Ousman <usman.traid@gmail.com>
2022-07-05 12:46:13 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#included "main.h"
+
+/**
+ * print_sign - plus or minus sign
+ * @n: the argument
+ *
+ * Return: 1 or 0 always
+ */
+
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		putchar('+');
+		result = 1;
+	} else if (n == 0)
+	{
+		putchar('0');
+		result = '0';
+	} else
+	{
+		putchar('-');
+		result = -1;
+	}
+	return (result);
+}
