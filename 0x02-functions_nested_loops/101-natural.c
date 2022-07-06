4285f7c5ee841d85a0f15d6d3a4b8fc8390dbcb7
Ousman <usman.traid@gmail.com>
2022-07-06 03:35:12 -0700

    natural sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,16 +4,16 @@
 /**
  * main -the process
  *
- * Return: sum of numbers
+ * Return: nothing
  */
 
-int main(void)
+void main(void)
 {
 	int i, sum;
 
+	sum = 0;
 	for (i = 0; i < 1024; i++)
 	{
-		sum = 0;
 		if ((i % 3 == 0) || (i % 5 == 0))
 			sum += i;
 	}
