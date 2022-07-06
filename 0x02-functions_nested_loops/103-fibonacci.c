240f21369adadd32904956a6b96369765397d854
Ousman <usman.traid@gmail.com>
2022-07-06 04:16:08 -0700

    sum even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	long int fib[50], sum = 2;
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (i = 2; i < 50; i++)
+	{
+		fib[i] = fib[i -1] + fib[i -2];
+		if (fib[i] % 2 == 0 && fib[i] <= 4000000)
+			sum += fib[i];
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
