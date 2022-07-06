5caddbfe4550506081a91b803547872b1eeae339
Ousman <usman.traid@gmail.com>
2022-07-05 17:31:20 -0700

    stopper

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,63 +1,34 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print_to_98 - main task
- * @c: starting point
+ * @s: starting point
+ *
  * Return: nothing
  */
-void print_to_98(int c)
-{
-	int a, b;
 
-	if (c <= 98)
+void print_to_98(int s)
+{
+	if (s <= 98)
 	{
-		for (a = c; a <= 98; a++)
+		for (; s <= 98, s++)
 		{
-			if (a < 0)
-			{
-				_putchar('-');
-				b = -1 * a;
-				if (b >= 100)
-				{
-					_putchar((b / 100) + '0');
-					_putchar(((b % 100) / 10) + '0');
-					_putchar((b % 10) + '0');
-				} else if (b > 10)
-				{
-					_putchar((b / 10) + '0');
-					_putchar((b % 10) + '0');
-				}else
-					_putchar((b % 10) + '0');
-			} else if (a < 10)
-				_putchar(a + '0');
-			else 
-			{
-				_putchar((a / 10) + '0');
-				_putchar((a % 10) +'0');
-			}
-			if (a != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d", s);
+			if (s == 98)
+				continue;
+			printf(", ");
 		}
-	} else 
+		printf("\n");
+	}else
 	{
-		for (a = c; a >= 98; a--)
+		for (; s >= 98, s--)
 		{
-			if (a >= 100)
-			{
-				_putchar((a / 100) + '0');
-				_putchar(((a % 100) / 10) + '0');
-			} else 
-				_putchar((a / 10) + '0');
-			_putchar((a % 10) + '0');
-			if (a != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d", s);
+			if (s != 98)
+				printf(", ");
 		}
+		printf("\n");
 	}
-	_putchar('\n');
 }
+
