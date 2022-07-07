93209daac322c82ded96a6a4242e237aade1db99
Ousman <usman.traid@gmail.com>
2022-07-07 05:35:16 -0700

    check if it is a number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * -isdigit - check if it is a natural number
+ *  @n: the input number
+ *
+ *  Return: Always 0 or 1
+ */
+
+int _isdigit(int n)
+{
+	int result;
+	if (n >= 48 && n <= 58)
+		result = 1;
+	else
+		result = 0;
+	return (result);
+}
