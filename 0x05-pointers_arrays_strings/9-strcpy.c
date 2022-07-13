da3e11f9be149704b4af3acb20f31f190677df25
Ousman <usman.traid@gmail.com>
2022-07-13 15:52:58 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy strings fromsource to destination
+ * @dest: the destination string
+ * @src: the source string
+ *
+ * Return: Always a pointer
+ */
+
+char _strcpy(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (result);
+}
