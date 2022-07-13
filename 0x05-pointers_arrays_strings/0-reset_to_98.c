cce90b0c88c25c005e5c68781dc577aa34982d59
Ousman <usman.traid@gmail.com>
2022-07-13 10:38:43 -0700

    resetting a parameter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - reset the value at the pointers it takes as argument to 98
- * @*p: the pointer is take as argument
+ * @p: the pointer is take as argument
  *
  * Return: nothing
  */
