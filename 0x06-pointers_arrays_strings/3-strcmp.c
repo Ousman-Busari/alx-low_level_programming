d1b60b9b88175b5d9d4b2f6708f6a6f8382dbc21
Ousman <usman.traid@gmail.com>
2022-07-14 12:03:33 -0700

    error fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,12 @@ int _strcmp(char *s1, char *s2)
 
 	while (*s1 && *s2)
 	{
-		if (*s1 == *s2)
-			i = 0;
-		else if (*s1 < *s2)
+		if (*s1 < *s2)
 			i = -15;
-		else
+		else if (*s1 > *s2)
 			i = 15;
-		*s1++;
-		*s2++;
+		s1++;
+		s2++;
 	}
 	return (i);
 }
