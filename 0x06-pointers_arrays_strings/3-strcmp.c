14c06f8238272aea0160900460327c1c413c5fd0
Ousman <usman.traid@gmail.com>
2022-07-14 11:38:04 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strins
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: ineteger
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while(*s1 && *s2)
+	{
+		if (*s1 == *s2)
+			i = 0;
+		else if (*s1 < *s2)
+			1 = -15;
+		else
+			i = 15;
+		s1++;
+		s2++;
+	}
+}
