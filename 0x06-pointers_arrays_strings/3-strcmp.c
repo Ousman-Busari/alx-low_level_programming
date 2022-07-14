5e87a7d861fa80364ea6d3a0737831d7091a61e6
Ousman <usman.traid@gmail.com>
2022-07-14 12:12:49 -0700

    p

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,14 +4,11 @@
  * _strcmp - compares two strins
  * @s1: first string
  * @s2: second string
- *
- * Return: ineteger
+ * Return: difference between the two strings
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
 	while (*s1 && *s2)
 	{
 		if (*s1 != *s2)
