189d1c96beeed966180f82500a5c2854331333dc
Ousman <usman.traid@gmail.com>
2022-07-15 04:18:37 -0700

    rot13 encode

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - rotate in 13 places
+ * @s: the string to rotate
+ * Return: the pointer to the resulting string
+ */
+
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while ((s[i] <= 'a' && s[i] >= 'z') ||
+				(s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] <= 'a' && s[i] >= 'm') ||
+					(s[i] >= 'A' && s[i] >= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+		}
+		i++;
+	}
+	return (s);
+}
