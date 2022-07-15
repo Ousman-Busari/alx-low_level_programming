053a5401f5577fe543a59a0bfd5d5e53f6dbc01c
Ousman <usman.traid@gmail.com>
2022-07-15 03:31:40 -0700

    leet code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - enocdes string
+ * @str: the tring to encode
+ *
+ * Return: pointer to the resulting dtring
+ */
+
+char leet(char *str)
+{
+	int i = 0, j;
+	char sub[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},
+	     cha[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	while(str[i])
+	{
+		for(j = 0; j < 10; j++)
+		{
+			if (str[i] == cha[j])
+				str[i] = sub[j];
+		}
+		i++;
+	}
+	resturn (str);
+}
