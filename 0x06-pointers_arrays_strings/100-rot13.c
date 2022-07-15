9581bd990280cd9b86add0c5f9259718e994da55
Ousman <usman.traid@gmail.com>
2022-07-15 04:39:31 -0700

    rot13 encode

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,9 +16,12 @@ char *rot13(char *s)
 		while ((s[i] <= 'a' && s[i] >= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] <= 'a' && s[i] >= 'm') || (s[i] >= 'A' && s[i] >= 'M'))
+			{
 				s[i] += 13;
-			else
-				s[i] -= 13;
+				continue;
+			}
+			s[i] -= 13;
+			break;
 		}
 		i++;
 	}
