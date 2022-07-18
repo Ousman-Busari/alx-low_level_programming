4a45ea33f9d28c981b19e550322b16fe23d06f99
Ousman-Busari <usman.traid@gmail.com>
2022-07-18 08:07:39 -0700

    local push

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory from src to src
+ * @dest: destination
+ * @src: where to copy from
+ * @n: number of bytes to copy
+ * Return: pointer to resulting dest
+ */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+  char *result = dest;
+
+  while (n--)
+  {
+    *dest = *src;
+    dest++, src++;
+  }
+  return (result);
+}
+  
