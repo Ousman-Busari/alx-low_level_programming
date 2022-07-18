39e1cd0fcd18abb09d4ba7deb53e42708b2fd545
Ousman-Busari <usman.traid@gmail.com>
2022-07-18 06:54:43 -0700

    local test

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _memset - assignsn bytes of a memory address to a certain value
+ * @s:  the memoty address
+ * @b: the char to assign to the specified memory space
+ * @n: number bytes to assign or set
+ * Return: pointer to the resulting memory address
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  int i;
+
+  i = 0;
+  while (i < n)
+    {
+      s[i] = b;
+      i++;
+    }
+  return (s);
+}
