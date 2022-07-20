3d2d684c56a1f9a333181a3cf460b22547d8b5ec
Ousman-Busari <usman.traid@gmail.com>
2022-07-20 12:02:02 -0400

    if in a recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s != '\0')
-		_puts_recursion(s);
-	if (*s == '\0')
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+
+	}
+	else
 		_putchar('\n');
 }
---
+++ 0x08-recursion/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char *s = "";
+	printf("size of s = %ld\n", sizeof(s));
+	printf("%c", s);
+	return (0);
+}
