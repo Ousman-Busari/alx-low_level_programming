41ed7e3b79de69569360d09a12b3cfb96cd1ad39
Ousman-Busari <usman.traid@gmail.com>
2022-07-21 10:54:08 -0400

    wild comparison

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * wildcmp - compare two strings
+ * @s1: the first string
+ * @s2: the secodns tring
+ * Return: 1 if they re the same, 0 otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)))
+		return (1);
+	return (0);
+}
