bc5fa24b8b660795bdd9c8dd0a2b58c59a6118e3
Ousman-Busari <usman.traid@gmail.com>
2022-07-21 06:37:12 -0400

    new

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,40 @@
 #include "main.h"
+#include <stdio.h>
+
+int _strlen_recursion(char *s);
 int cmp_char(char *s, int len);
+
+/**
+ *_strlen_recursion - determines the length of the string s
+ * @s: the input string
+ * Return: the length of string s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
+
+/**
+ * cmp_char - comapre characters at different position
+ * @s: the input string
+ * @len: position of the character to compare
+ *
+ * Return: reversed string, one char at a time
+ */
+
+int cmp_char(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	if (*s == *(s + len - 1))
+		cmp_char(s + 1, len - 2);
+	return (0);
+}
+
 
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -17,20 +52,3 @@ int is_palindrome(char *s)
 	return (cmp_char(s, len));
 }
 
-/**
- * cmp_char - comapre characters at different position
- * @s: the input string
- * @len: position of the character to compare
- *
- * Return: reversed string, one char at a time
- */
-
-int cmp_char(char *s, int len)
-{
-	if (len <= 1)
-		return (1);
-	if (*s == *(s + len - 1))
-		cmp_char(s + 1, len - 2);
-	else
-		return (0);
-}
