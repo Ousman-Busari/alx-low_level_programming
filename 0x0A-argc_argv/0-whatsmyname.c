0332c9060bedbd9b0074e59d37a2858fbe18e992
Ousman-Busari <usman.traid@gmail.com>
2022-07-23 06:16:27 -0400

    error fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,10 @@
  * main - prints out the name of the program
  * @argc: the number of arguments
  * @argv: array of string containing the command line arguments
- * Return: integer
+ * Return: nothing
  */
 
-int main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (0);
 }
