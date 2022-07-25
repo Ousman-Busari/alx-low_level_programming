09be5f39932b8259335206545621b900c984da7b
Ousman-Busari <usman.traid@gmail.com>
2022-07-25 14:40:59 -0400

    varibale-size array from malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,11 +16,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (NULL);
-	else
-	{
-		result = malloc(sizeof(char) * size);
-		for (i = 0; i < size; ++i)
-			result[i] = c;
-		return (result);
-	}
+	result = malloc(sizeof(char) * size);
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < size; ++i)
+		result[i] = c;
+	return (result);
 }
