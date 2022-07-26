454b96e73ad410803589192275750b5083e75e49
Ousman-Busari <usman.traid@gmail.com>
2022-07-26 12:46:41 -0400

    error fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,11 +19,7 @@ int **alloc_grid(int width, int height)
 
 	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
-	{
-		for (i = 0; i < height; i++)
-			free(arr[i]);
 		return (NULL);
-	}
 
 	for (i = 0; i < height; i++)
 	{
