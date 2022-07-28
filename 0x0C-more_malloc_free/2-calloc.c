58369ba006706d80b3c8782f8cd9896ff61e954c
Ousman-Busari <usman.traid@gmail.com>
2022-07-28 13:23:41 -0400

    calloc type of fucntion

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *arr;
+	char *arr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,5 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 		return (NULL);
 
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
 	return (arr);
 }
