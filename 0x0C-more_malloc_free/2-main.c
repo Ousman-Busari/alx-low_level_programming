3ad3959b81486820acb7c46ae664170a483976f4
Ousman-Busari <usman.traid@gmail.com>
2022-07-28 13:27:35 -0400

    calloc type

--- 0x0C-more_malloc_free/2-main.c
+++ 0x0C-more_malloc_free/2-main.c
@@ -40,11 +40,11 @@ int main(void)
 {
 	char *a;
 
-	a = _calloc(10, sizeof(char));
+	a = _calloc(98, sizeof(char));
 	strcpy(a, "Best");
 	strcpy(a + 4, " School! :)\n");
 	a[97] = '!';
 	simple_print_buffer(a, 98);
-	free(a);
+/*	free(a);*/
 	return (0);
 }
