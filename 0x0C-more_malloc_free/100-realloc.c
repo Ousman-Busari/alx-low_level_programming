7a0512b60c622b6d958e8ce1dc3d75f15fa561b7
Ousman-Busari <usman.traid@gmail.com>
2022-07-28 17:28:26 -0400

    realloc type

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc -  reallocates a memory block using malloc and free
+ * @old_size: old size of the of the allocated memory
+ * @new_size: new size of the allocated memory
+ *
+ * Return: pointer to the new memory, or NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr, *_ptr;
+	int i = 0, min;
+
+	_ptr = ptr;
+	if (new_size == old_size)
+		return (_ptr);
+	if (new_size == 0)
+	{
+		free(_ptr);
+		return (NULL);
+	}
+	if (_ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
+	if (new_size != old_size)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+	}
+	if (old_size < new_size)
+		min = old_size;
+	else
+		min = new_size;
+	for (; i < min; i++)
+	{
+		new_ptr[i] = _ptr[i];
+	}
+	return (new_ptr);
+}
