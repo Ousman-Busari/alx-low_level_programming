2192eacb77fd4ea56ae581f73635b5a643c64e43
Ousman-Busari <usman.traid@gmail.com>
2022-07-28 14:12:23 -0400

    calloc type with array of int

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,12 +16,12 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min));
+	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
 
 	min_initial = min;
-	for (i = 0; i < (max - min_initial); i++)
-		     arr[i] = min++;
+	for (i = 0; i <= (max - min_initial); i++)
+		arr[i] = min++;
 	return (arr);
 }
