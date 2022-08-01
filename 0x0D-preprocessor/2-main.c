6b0cd50e39b5e8dc8c40fdfc07072a6c36ee2d26
Ousman-Busari <usman.traid@gmail.com>
2022-08-01 08:50:16 -0400

    predefined macros

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/* main - prints the name of the file it was compiled from
+/*
+ * main - prints the name of the file it was compiled from
  *
  * Return: Always Success
  */
 
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
