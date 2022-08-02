76faf762fa6f71d8b5e160159019fea5bffe3044
Ousman-Busari <usman.traid@gmail.com>
2022-08-02 05:34:40 -0400

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - create a dog object
+ * @name: name of the dog
+ * @age: how old the dog is
+ * @owner: who owns the dog
+ *
+ * Description: defines a dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
