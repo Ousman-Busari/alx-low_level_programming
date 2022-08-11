a484cf104a188f17ce6ff5818035e1b47365685b
Ousman-Busari <usman.traid@gmail.com>
2022-08-11 19:46:17 -0400

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,13 +11,12 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	temp = head;
-	while (temp->next != NULL)
+	while (head)
 	{
+		temp = head;
+		head = head->next;
 		free(temp->str);
 		free(temp);
-		temp = temp->next;
 	}
-	free(temp->str);
-	free(temp);
+	free(head);
 }
