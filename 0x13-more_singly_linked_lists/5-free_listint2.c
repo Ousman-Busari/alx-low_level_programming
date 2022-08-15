83e550c13abd4bc841c284815779bca716dc6b61
Ousman-Busari <usman.traid@gmail.com>
2022-08-15 17:20:23 -0400

    no head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,7 +12,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
@@ -20,6 +20,6 @@ void free_listint2(listint_t **head)
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
-	};
+	}
 	*head = NULL;
 }
