4b8fdcacd5848ced2b4af0c930cf203f9a87c73f
Ousman-Busari <usman.traid@gmail.com>
2022-08-15 18:20:20 -0400

    fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,13 +22,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	if (temp == NULL)
 		return (NULL);
 	temp1 = *head;
-/*
- *	while (temp1 = temp1->next)
+	while (temp1 = temp1->next)
 		temp1 = temp1->next;
 		c++;
 	if (index > c - 1)
 	return (NULL);
-*/
 	temp->n = n;
 	if (*head == NULL)
 	{
