b13ee3de5bc4123afb184a82422c93c23f8fe1f2
Ousman-Busari <usman.traid@gmail.com>
2022-08-20 06:16:41 -0400

    manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,8 +9,9 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int in = 0, max = 32768;
+	unsigned long int in = 0, max = 9223372036854775807;
 
+	max = ~max;
 	if (n == 0)
 	{
 		_putchar('0');
