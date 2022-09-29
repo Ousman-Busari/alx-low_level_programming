21a2df83aa81116e1201b58e4dfb9cb1ab9cb9e5
Ousman-Busari <usman.traid@gmail.com>
2022-09-29 14:40:02 -0400

    typo fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->n = n;
 			new_node->next = temp;
 			new_node->prev = temp->prev;
-			new_node->prev->next = new_node;
+			temp->prev->next = new_node;
 			temp->prev = new_node;
 			return (new_node);
 		}
