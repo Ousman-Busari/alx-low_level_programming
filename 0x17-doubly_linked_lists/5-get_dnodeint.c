883095acfa931a8e46759d384e9a67b48b7c04ec
Ousman-Busari <usman.traid@gmail.com>
2022-09-29 11:07:33 -0400

    in-file function

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+size_t dlistint_len(const dlistint_t *head);
+
 /**
  * get_dnodeint_at_index - gets the node at the nth index of a dlinked list
  * @head: head pointer of the doubly-linked list
@@ -24,4 +26,27 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		i++;
 	}
 	return (head);
+}
+
+
+/**
+ * dlistint_len - returns the number of elements in a doubly-linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: number of elements in the list, 0 if NULL
+ */
+
+size_t dlistint_len(const dlistint_t *head)
+{
+	int counter = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		counter++;
+		head = head->next;
+	}
+
+	return (counter);
 }
