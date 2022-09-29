d52cbdd77bc5d7447502a6f1283d0872177da82b
Ousman-Busari <usman.traid@gmail.com>
2022-09-29 12:25:01 -0400

    refined

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,5 @@
 #include "lists.h"
 
-size_t dlistint_len(const dlistint_t *head);
-
 /**
  * insert_dnodeint_at_index - inserts a node with a given data(n)
  * at a given index(idx)
@@ -16,65 +14,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx,
 				     int n)
 {
 	size_t i = 0;
-	size_t dlist_len = dlistint_len(*head);
-	dlistint_t *temp, *temp1, *new_node;
+	dlistint_t *temp, *new_node;
 
-	if (idx > dlist_len || head == NULL)
-		return (NULL);
 	if (idx == 0)
-	{
-		new_node = add_dnodeint(head, n);
-		return (new_node);
-	}
-
-	if (idx == dlist_len)
-	{
-		new_node = add_dnodeint_end(head, n);
-		return (new_node);
-	}
+		return (add_dnodeint(head, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	new_node->prev =  NULL;
-	new_node->next = NULL;
 
 	temp = *head;
 	while (temp)
 	{
-		if (i == (idx - 1))
-			break;
+		if (i == idx)
+		{
+		       	new_node->next = temp;
+	       		new_node->prev = temp->prev;
+			new_node->prev->next = new_node;
+			temp->prev = new_node;
+			return (new_node);
+		}
 		temp = temp->next;
 		i++;
 	}
-	temp1 = temp->next;
-	temp->next = new_node;
-	new_node->prev = temp;
-	new_node->next = temp1;
-	temp1->prev = new_node;
-	return (new_node);
-}
 
-
-/**
- * dlistint_len - returns the number of elements in a doubly-linked list
- * @head: pointer to the head of the list
- *
- * Return: number of elements in the list, 0 if NULL
- */
-
-size_t dlistint_len(const dlistint_t *head)
-{
-	int counter = 0;
-
-	if (head == NULL)
-		return (0);
-	while (head)
-	{
-		counter++;
-		head = head->next;
-	}
-
-	return (counter);
+       	if (idx == i)
+		return (add_dnodeint_end(head, n));
+	return (NULL);
 }
