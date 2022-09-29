d07af58ed984b193a0d1653af16544f88698e768
Ousman-Busari <usman.traid@gmail.com>
2022-09-29 14:23:11 -0400

    null ends fixed

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,33 +14,24 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *temp;
 
-	if (*head == NULL)
-		return (-1);
-
-	if (index == 0)
-	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
-		return (1);
-	}
-
 	temp = *head;
 	while (temp)
 	{
 		if (i == index)
 		{
-			temp->prev->next = temp->next;
-			temp->next->prev = temp->prev;
+			if (temp->prev != NULL)
+				temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			if (index == 0)
+				*head = temp->next;
+
 			free(temp);
 			return (1);
 		}
 		temp = temp->next;
 		i++;
 	}
-
-	if (index >= i)
-		return (-1);
 
 	return (-1);
 }
