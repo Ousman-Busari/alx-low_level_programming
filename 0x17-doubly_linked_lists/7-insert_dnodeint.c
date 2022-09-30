5d165677b7e8047b096574db51e6ad1b30844406
Ousman-Busari <usman.traid@gmail.com>
2022-09-30 07:07:42 -0400

    fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,9 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *temp, *new_node;
 
-	if ((idx != 0) && (*h == NULL))
-		return (NULL);
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
