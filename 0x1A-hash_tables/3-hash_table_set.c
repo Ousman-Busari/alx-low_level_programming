ac32e5d33b4eb83c8da2240413e0207bb20aeae8
Ousman-Busari <usman.traid@gmail.com>
2022-10-22 05:25:58 -0400

    bucket search fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,36 +10,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_ele;
+	hash_node_t *new_ele, *tmp;
 	unsigned long int index;
+	char *value_dup;
 
 	if (!key || !value || !ht)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	value_dup = strdup(value);
+	if (!value_dup)
+		return (0);
 
+/*
+ * check a bucket(list of all diffrent keys with equal index) if a particular
+ * key already exist
+*/
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = value_dup;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 	new_ele = malloc(sizeof(hash_node_t *));
 	if (!new_ele)
 		return (0);
-
 	new_ele->key = strdup(key);
 	if (!new_ele->key)
-		return (0);
-	new_ele->value = strdup(value);
-	if (!new_ele->value)
 	{
-		free(new_ele->key);
+		free(value_dup), free(new_ele);
 		return (0);
 	}
-	new_ele->next = NULL;
-
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_ele;
-		return (1);
-	}
-
+	new_ele->value = value_dup;
 	new_ele->next = ht->array[index];
 	ht->array[index] = new_ele;
-       	return (1);
+	return (1);
 }
