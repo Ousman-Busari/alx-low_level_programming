20a7147fa98c931b81b33b455c2222ea67c18893
Ousman <usman.traid@gmail.com>
2023-01-27 01:14:18 -0500

    requirement fixed

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -19,7 +19,7 @@ int interpolation_search(int *array, size_t size, int value)
 
 	for (low = 0;;)
 	{
-		pos = low + (((double)(high - low)/ (array[high] - array[low]))
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
 			     * (value - array[low]));
 
 		if (pos < size)
